fc7f8443c8b75c2e9448deafe65279b8f78f7a39
yacob <yacob.wijaya@bitozen.id>
2017-12-11 21:14:41 +0700

    Separate profile object in app-sidebar-header

--- src/app/components/app-sidebar-header/app-sidebar-header.component.ts
+++ src/app/components/app-sidebar-header/app-sidebar-header.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
     './app-sidebar-header.component.scss'
   ]
 })
-export class AppSidebarHeaderComponent { }
+
+export class AppSidebarHeaderComponent {
+  public profile: Profile = {
+    photoPath: 'http://www.saparch.com/public/widget/1_widget_john-doe.png';
+    name: 'Adrian D. Pantulu',
+    title: 'Delivery Manager SR2'
+  }
+}
+
+// Object: Profile
+export class Profile {
+  public photoPath: string;
+  public name: string;
+  public title: string;
+}
